c9fcf41a890734ea4b89a1230dcaabd90e039fb4
Luke Sturgill <lukesturgill@gmail.com>
2022-06-20 16:20:41 -0400

    added types to typeDef.js

--- server/schemas/typeDef.js
+++ server/schemas/typeDef.js
@@ -3,9 +3,48 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
-    type Query{
-        helloWorld: String
+    type User{
+        _id: ID
+        username: String
+        email: String
+        password: String
+        savedBooks: [Book]
     }
+
+    type Book{
+        _id: ID
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+
+    type SaveBookInput{
+        description: String
+        title: String
+        bookId: String
+        image: String
+        link: String
+        authors: [String]
+    }
+
+    type Query{
+        user: User
+    }
+
+    type Mutation{
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, password: String!, email: String!): Auth
+        saveBook(body: saveBookInput): User
+        deleteBook(userId: ID!, bookId: String!): User
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+      }
 `;
 
 // export the typeDefs
