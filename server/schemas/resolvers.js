7c4384a76e17ac652de0dd6aefc924acdae31e4c
Luke Sturgill <lukesturgill@gmail.com>
2022-06-29 02:39:56 -0400

    finished resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -33,6 +33,28 @@ const resolvers = {
             const token = signToken(user);
             return { token, user };
         },
+        saveBook: async (parent, {input}, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: input } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('You need to be logged in')
+        },
+        removeBook: async (parent, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: args.bookId} } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('You need to be logged in')
+        }
     },
 };
 
